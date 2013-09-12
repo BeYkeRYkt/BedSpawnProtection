558901142b4d3987e5d50464ae8abfcabfcdc045
BeYkeRYkt <beykerykt@gmail.com>
2013-09-12 18:49:57 +1000

    Update 1.1DinnerEditon "R"

--- src/com/gmail/xtendspb/plugins/bedspawnprotection/BedSpawnProtection.java
+++ src/com/gmail/xtendspb/plugins/bedspawnprotection/BedSpawnProtection.java
@@ -2,6 +2,7 @@ package com.gmail.xtendspb.plugins.bedspawnprotection;
 
 
 import java.io.File;
+import java.util.List;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -21,15 +22,16 @@ public class BedSpawnProtection extends JavaPlugin{
 				    "\nOriginal Code: XtenD" +
 					"\nModded Dinnerspond_");
 				fc.addDefault("Radius-protection", 8);
+				fc.addDefault("Potion-effect-enable", true);
 				fc.addDefault("Potion-amount", "ONE");
-				fc.addDefault("Potion-effect-one-enable", true);
-				fc.addDefault("Potion-effect-list-enable", false);
-				fc.addDefault("Potion-effect-name", "");
+				fc.addDefault("Potion-effect-name", "CONFUSION");
 				fc.addDefault("Potion-effect-time", 60);
 				fc.addDefault("Potion-effect-level", 2);
 				//StringList
-				fc.createSection("Potion-list");
-				fc.set("Potion-list", "CONFUSION");
+				List<String> list = fc.getStringList("Potion-list");
+				list.add("CONFUSION");
+				list.add("BLINDNESS");
+                                fc.set("Potion-list", list);
 				//End
 				fc.addDefault("Locale.warning-killer", "Не убивай игроков возле их кровати!");
 				
@@ -43,12 +45,8 @@ public class BedSpawnProtection extends JavaPlugin{
 		//Register Events
 		getServer().getPluginManager().registerEvents(new BedSpawnProtectionListener(), this);
 		
-		//Logger
 		this.getLogger().info("Plugin is Enabled!");
-		this.getLogger().info("Orignal code: XtenD");
-		this.getLogger().info("Modded: Dinnerspond_");
 	}
 
 
 }
-
